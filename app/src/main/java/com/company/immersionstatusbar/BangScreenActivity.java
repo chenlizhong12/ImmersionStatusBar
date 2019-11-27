9cea0c6f5ca4cf1fde163ecf74185f9c520b37a4
chenlizhong12 <472874481@qq.com>
2019-11-27 15:08:57 +0800

    沉浸式状态栏和刘海屏适配

---
+++ app/src/main/java/com/company/immersionstatusbar/BangScreenActivity.java
@@ -0,0 +1,64 @@
+package com.company.immersionstatusbar;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.DisplayCutout;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowInsets;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BangScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_bang_screen);
+        View decodeView = getWindow().getDecorView();
+        int options = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decodeView.setSystemUiVisibility(options);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+        if (Build.VERSION.SDK_INT >= 28) {
+            WindowManager.LayoutParams layoutParams = getWindow().getAttributes();
+            layoutParams.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+            getWindow().setAttributes(layoutParams);
+        }
+    }
+
+    //适配刘海屏遮住控件显示，进行适配
+    private void matchScreen() {
+        final FrameLayout frameLayout = findViewById(R.id.rootLayout);
+        final Button topButton = findViewById(R.id.topButton);
+        final Button side = findViewById(R.id.sideButton);
+        frameLayout.setOnApplyWindowInsetsListener(new View.OnApplyWindowInsetsListener() {
+            @Override
+            public WindowInsets onApplyWindowInsets(View v, WindowInsets insets) {
+                if (Build.VERSION.SDK_INT >= 28) {
+                    DisplayCutout displayCutout = insets.getDisplayCutout();
+                    if (displayCutout != null) {
+                        int right = displayCutout.getSafeInsetRight();
+                        int left = displayCutout.getSafeInsetLeft();
+                        int bottom = displayCutout.getSafeInsetBottom();
+                        int top = displayCutout.getSafeInsetTop();
+                        FrameLayout.LayoutParams topParams = (FrameLayout.LayoutParams) topButton.getLayoutParams();
+                        topParams.setMargins(left, top, right, bottom);
+                        FrameLayout.LayoutParams sideParams = (FrameLayout.LayoutParams) side.getLayoutParams();
+                        sideParams.setMargins(left, top, right, bottom);
+                    }
+
+                }
+                return insets.consumeSystemWindowInsets();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/company/immersionstatusbar/MainActivity.java
@@ -0,0 +1,72 @@
+package com.company.immersionstatusbar;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        transparentStatus();
+//        transparentNavigationAndStatusBar();
+    }
+
+    //隐藏状态栏和actionBar
+    private void hideStatusBar() {
+        View decorView = getWindow().getDecorView();
+        int option = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(option);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+    }
+
+    //状态栏透明和隐藏actionBar
+    private void transparentStatus() {
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+    }
+
+    //透明导航栏和状态栏
+    private void transparentNavigationAndStatusBar() {
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            getWindow().setNavigationBarColor(Color.TRANSPARENT);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus && Build.VERSION.SDK_INT >= 19) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
+
+}
